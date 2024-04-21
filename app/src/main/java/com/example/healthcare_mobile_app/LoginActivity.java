dcf7952770d342633972d30663fc6d4212b90c1b
Abhishek Singh <singh.abhishek.offc@gmail.com>
2024-04-21 13:24:23 +0530

    Home Page

--- app/src/main/java/com/example/healthcare_mobile_app/LoginActivity.java
+++ app/src/main/java/com/example/healthcare_mobile_app/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.healthcare_mobile_app;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -37,10 +39,21 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username = edUsername.getText().toString();
                 String password = edPassword.getText().toString();
+                Database db = new Database(getApplicationContext(), "Healthcare", null, 1);
                 if (username.length()==0 || password.length()==0){
                     Toast.makeText(getApplicationContext(),"Please fill All the details",Toast.LENGTH_SHORT).show();
                 }else {
-                    Toast.makeText(getApplicationContext(), "Login Success",Toast.LENGTH_SHORT).show();
+                    if (db.login(username,password)==1){
+                        Toast.makeText(getApplicationContext(), "Login Success",Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedPreferences = getSharedPreferences("shared prefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username", username);
+
+                        editor.apply();
+                        startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Invalid Username and Password",Toast.LENGTH_SHORT).show();
+                    }
 
                 }
             }
