6423b0f921326c247b2c41decbc39dbb2de96c8e
Abhishek Singh <singh.abhishek.offc@gmail.com>
2024-04-21 10:41:06 +0530

    till register page

---
+++ app/src/main/java/com/example/healthcare_mobile_app/Database.java
@@ -0,0 +1,30 @@
+package com.example.healthcare_mobile_app;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+public class Database extends SQLiteOpenHelper {
+    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+        @Override
+        public void onCreate(SQLiteDatabase sqliteDatabase) {
+            String qry1 = "create table users(username text, email text, password text)";
+            sqliteDatabase.execSQL(qry1);
+        }
+        @Override
+        public void onUpgrade (SQLiteDatabase sqliteDatabase, int i, int il) {
+
+        }
+        public void register (String username, String email, String password){
+            ContentValues cv = new ContentValues();
+            cv.put("username", username);
+            cv.put("email", email);
+            cv.put("password", password);
+            SQLiteDatabase db = getWritableDatabase();
+            db.insert("users", null, cv);
+            db.close();
+        }
+    }
---
+++ app/src/main/java/com/example/healthcare_mobile_app/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.healthcare_mobile_app;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNewUser);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if (username.length()==0 || password.length()==0){
+                    Toast.makeText(getApplicationContext(),"Please fill All the details",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Login Success",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
