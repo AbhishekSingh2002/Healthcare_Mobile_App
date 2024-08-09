4affed331938eb114b3cdb723b90dbedf9f84c0f
Abhishek Singh <singh.abhishek.offc@gmail.com>
2024-08-09 13:40:23 +0530

    updating deps

--- app/src/main/java/com/example/healthcare_mobile_app/LoginActivity.java
+++ app/src/main/java/com/example/healthcare_mobile_app/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.example.healthcare_mobile_app;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -24,33 +27,34 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-
         edUsername = findViewById(R.id.editTextLoginUsername);
         edPassword = findViewById(R.id.editTextLoginPassword);
         btn = findViewById(R.id.buttonLogin);
         tv = findViewById(R.id.textViewNewUser);
 
         btn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 String username = edUsername.getText().toString();
                 String password = edPassword.getText().toString();
-                if (username.length()==0 || password.length()==0){
-                    Toast.makeText(getApplicationContext(),"Please fill All the details",Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(getApplicationContext(), "Login Success",Toast.LENGTH_SHORT).show();
-
+                Database db = new Database(getApplicationContext(), "healthcare", null, 1);
+                if (username.length() == 0 || password.length() == 0){
+                    Toast.makeText(getApplicationContext(), "Please fill All details", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (db.login(username, password)==0){
+                        Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username", username);
+                        editor.apply();
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Invalid Username and Password", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
-
-        });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+            }});
 
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,3 +64,6 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 }
+
+
+
